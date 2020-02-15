23bc117639683b7a0a505010066d1eb5140fd175
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2020-02-14 17:39:25 -0800

    Same
    Same

--- Bash Scripts/DataTransfer.sh
+++ Bash Scripts/DataTransfer.sh
@@ -2,7 +2,7 @@
 echo "----------------------------------------------------------------------------"
 echo "This script is designed to transfer user data as efficiently as possible."
 echo "*Note* this script will not transfer any library data or any iCloud data."
-echo "This script will also not transfer any extra items in the root of the profile"
+echo "This script will also not transfer any extra directories in the root of the profile"
 echo "Created by Davis Henckel 10/21/2019"
 echo "Most recent update 01/29/2020"
 echo "----------------------------------------------------------------------------"
