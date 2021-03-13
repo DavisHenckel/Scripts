66bddf983a84a96b56bd7a4d6d7de3fbb452ab93
DavisHenckel <davishenckel@hotmail.com>
2021-03-13 08:50:09 -0800

    Added further input validation

--- Powershell Scripts/CollectInput.ps1
+++ Powershell Scripts/CollectInput.ps1
@@ -1,6 +1,6 @@
 
 
-Function GetUserInput { #Collects user input based off template sent from HR
+Function GetUserInput { #Collects user input based off template sent from HR This will not collect lines with less than 2 characters to avoid collecting empty lines.
     $InputParams = [System.Collections.ArrayList]@()
     Write-Host("Enter the user's information (Enter 2 blank lines to stop collecting input):")
     $UserInput = $null
@@ -25,15 +25,70 @@ Function GetUserInput { #Collects user input based off template sent from HR
     return $InputParams
 }
 
-Function UserInputToParameters ($UserInfoArgs) { #Interprets the user input and puts them in a new arraylist
+Function ValidateInput ($UserInfoArgs) { #ensures input given is in a valid format. After this function is run, there should only be 5 Entries in the array list
+
+    for ($i = 0; $i -lt $UserInfoArgs.Count; $i++) { #If a line is not valid or doesn't contain what we are looking for. Remove it from the list entirely and decrement counter
+        if ($i -eq 0 -and ($UserInfoArgs[$i] -Match "Dept" -eq 0 -or $UserInfoArgs[$i] -Match "Location" -eq 0)) { #validate first line
+            $UserInfoArgs.Remove($UserInfoArgs[$i])
+            if ($i -eq $UserInfoArgs.Count) {
+                Write-Host "Never found Location Number... Check format and try again"
+                exit
+            }
+            $i--
+            continue
+        }
+        if ($i -eq 1 -and ($UserInfoArgs[$i] -Match "Employee Number" -eq 0)) { #validate second line
+            $UserInfoArgs.Remove($UserInfoArgs[$i])
+            if ($i -eq $UserInfoArgs.Count) {
+                Write-Host "Never found Employee Number... Check format and try again"
+                exit
+            }
+            $i--
+            continue
+        }
+        if ($i -eq 2 -and ($UserInfoArgs[$i] -Match "User Name" -eq 0)) { #validate third line
+            $UserInfoArgs.Remove($UserInfoArgs[$i])
+            if ($i -eq $UserInfoArgs.Count) {
+                Write-Host "Never found User Name... Check format and try again"
+                exit
+            }
+            $i--
+            continue
+        }
+        if ($i -eq 3 -and ($UserInfoArgs[$i] -Match "Preferred Name for Email" -eq 0)) { #validate fourth line
+            $UserInfoArgs.Remove($UserInfoArgs[$i])
+            if ($i -eq $UserInfoArgs.Count) {
+                Write-Host "Never found Prefered name for Email... Check format and try again"
+                exit
+            }
+            $i--
+            continue
+        }
+        if ($i -eq 4 -and ($UserInfoArgs[$i] -Match "Job Title" -eq 0)) { #validate fifth line
+            $UserInfoArgs.Remove($UserInfoArgs[$i])
+            if ($i -eq $UserInfoArgs.Count) {
+                Write-Host "Never found Job Title... Check format and try again"
+                exit
+            }
+            $i--
+            continue
+        }
+        elseif ($i -gt 4) {
+            $UserInfoArgs.Remove($UserInfoArgs[$i]) #remove the line since these are the only 5 that will enter data into AD.
+        }                
+    }
+    return $UserInfoArgs
+}
+
+Function UserInputToParameters ($UserInfoArgs) { #Interprets the user input and puts them in a new arraylist. Also helps to validate input more specifically.
     $ModifiedParams = [System.Collections.ArrayList]@()
-    for ($i = 0; $i -lt $UserInfoArgs.Length; $i++) {
+    for ($i = 0; $i -lt $UserInfoArgs.Count; $i++) {
         if ($i -eq 0) {
             $UserInfoArgs[$i] = $UserInfoArgs[$i].Replace("Dept / Location: ","")
             $UserInfoArgs[$i] = $UserInfoArgs[$i].Replace(" ","") #Reomve extra space if it exists
             $UserInfoArgs[$i] = $UserInfoArgs[$i].SubString(0,3) #take only next 3 chars
             if ($UserInfoArgs[$i].Length -ne 3) {
-                Write-Host ("ERROR, Location Code is not 3 digits or contains invalid characters. Enter the correct Location Code:`n")
+                Write-Host ("ERROR, Location Code is not 3 digits. Enter the correct Location Code:`n")
                 $UserInfoArgs[$i] = Read-Host
             }
             $ModifiedParams.Add($UserInfoArgs[$i]) | Out-Null #prevent adding integers
@@ -42,7 +97,7 @@ Function UserInputToParameters ($UserInfoArgs) { #Interprets the user input and 
             $UserInfoArgs[$i] = $UserInfoArgs[$i].Replace("Employee Number:","")
             $UserInfoArgs[$i] = $UserInfoArgs[$i].Replace(" ","") #Reomve extra space if it exists
             if ($UserInfoArgs[$i].Length -ne 4) {
-                Write-Host ("ERROR, Employee ID is not 4 digits or contains invalid characters. Enter the correct Employee ID:`n")
+                Write-Host ("ERROR, Employee ID is not 4 digits. Enter the correct Employee ID:`n")
                 $UserInfoArgs[$i] = Read-Host 
             }
             $ModifiedParams.Add($UserInfoArgs[$i]) | Out-Null #prevent adding integers
@@ -52,12 +107,12 @@ Function UserInputToParameters ($UserInfoArgs) { #Interprets the user input and 
             $ModifiedParams.Add($UserInfoArgs[$i]) | Out-Null #prevent adding integers
         }
         if ($i -eq 3) {
-            if ($UserInfoArgs[$i] -eq "Preferred Name for Email: " -or  $UserInfoArgs[$i] -eq "Preferred Name for Email:  ") {
+            $UserInfoArgs[$i] = $UserInfoArgs[$i].Replace(" ","") #Reomve spaces if they exist
+            if ($UserInfoArgs[$i] -eq "PreferredNameforEmail:") {
                 continue
             }
             else {
                 $UserInfoArgs[$i] = $UserInfoArgs[$i].Replace("Preferred Name for Email: ","")
-                $UserInfoArgs[$i] = $UserInfoArgs[$i].Replace(" ","") #Reomve extra space if it exists
                 $UserInfoArgs[$i] = $UserInfoArgs[$i].Replace("."," ") #Replace . with space to fit name format    
                 $UserInfoArgs[$i] = $UserInfoArgs[$i].SubString(0, $UserInfoArgs[$i].IndexOf('@'))
                 $ModifiedParams[2] = $UserInfoArgs[$i] # set name to be the email name.                
@@ -77,14 +132,14 @@ Function ConfirmInputCorrect($ModifiedParams) {
         Write-Host("Data Collected:`n================`n ")
         for ($i = 0; $i -lt 4; $i++) {
             switch ($i) {
-                0 { $temp = $ModifiedParams[$i]
-                    Write-Host("Location Code:`n$temp`n")}
-                1 { $temp = $ModifiedParams[$i]
-                    Write-Host("Employee ID:`n$temp`n")}
-                2 { $temp = $ModifiedParams[$i]
-                    Write-Host("User Name:`n$temp`n")}
-                3 { $temp = $ModifiedParams[$i]
-                    Write-Host("Job Title:`n$temp`n")}
+                0 { $ToPrint = $ModifiedParams[$i]
+                    Write-Host("Location Code:$ToPrint`n")}
+                1 { $ToPrint = $ModifiedParams[$i]
+                    Write-Host("Employee ID:$ToPrint`n")}
+                2 { $ToPrint = $ModifiedParams[$i]
+                    Write-Host("User Name:$ToPrint`n")}
+                3 { $ToPrint = $ModifiedParams[$i]
+                    Write-Host("Job Title:$ToPrint`n")}
                 default { Write-Host("This won't ever happen")}
             }
         }
@@ -98,13 +153,14 @@ Function ConfirmInputCorrect($ModifiedParams) {
             exit
         }
         Clear-Host
-        Write-Host("Invalid Input. Enter (y/n)")
+        Write-Host("Invalid Input. Enter (y/n)`n")
     }
     return $UserInput
 } 
 Function Main { #Main function -- Don't need this. Just for my organization
-    $UserInfoData = GetUserInput
-    $ModifiedParams = UserInputToParameters($UserInfoData)
+    [System.Collections.ArrayList]$UserInfoData = GetUserInput
+    [System.Collections.ArrayList]$ModifiedData = ValidateInput($UserInfoData)
+    [System.Collections.ArrayList]$ModifiedParams = UserInputToParameters($ModifiedData)
     $Proceed = ConfirmInputCorrect($ModifiedParams)
     if ($Proceed -eq 'y' -or $Proceed -eq 'Y') {
         #Call newUserScript and pass $ModifiedParams[0], $ModifiedParams[1], $ModifiedParams[2], $ModifiedParams[3]
